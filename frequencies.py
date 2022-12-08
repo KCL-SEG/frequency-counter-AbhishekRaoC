8c2e066f43ac159bd95f3e4d9db09afa7d694fa1
Abhishek Rao Chimbili <abhishekraochimbili@gmail.com>
2022-12-08 14:52:57 +0000

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,12 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for i in items:
+        stringed = str(i)
+        if stringed not in frequencies.keys():
+            frequencies[stringed] = 1
+        else:
+            val = frequencies.get(stringed) + 1
+            frequencies.update({stringed : val})
+
     return frequencies
